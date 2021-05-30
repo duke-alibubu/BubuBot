214c324750a69a4a0cdfe7a7952c5037c4328767
duke-alibubu <ttdungalibubu@gmail.com>
2021-05-30 23:02:37 +0800

    new images for tit & ass

--- bububot.py
+++ bububot.py
@@ -236,7 +236,7 @@ async def ass(ctx):
     if not ctx.channel.is_nsfw():
         await ctx.send("Sorry pal, you can only use this command in a NSFW channel.")
         return
-    MAX = 16
+    MAX = 17
     img_path = f'ass/{randrange(MAX) + 1}.jpg'
     img_url = storage.child(img_path).get_url(None)
     await ctx.send("Ah I see, you are a man of culture as well.")
@@ -247,7 +247,7 @@ async def ass(ctx):
     if not ctx.channel.is_nsfw():
         await ctx.send("Sorry pal, you can only use this command in a NSFW channel.")
         return
-    MAX = 21
+    MAX = 25
     img_path = f'tits/{randrange(MAX) + 1}.jpg'
     img_url = storage.child(img_path).get_url(None)
     await ctx.send("Remember: In the end, Boobs - are nothing more than fake asses.")
